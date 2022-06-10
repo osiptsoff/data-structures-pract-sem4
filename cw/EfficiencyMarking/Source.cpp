c0fb6978587d63901e43c36983804d4727c1fb5c
Никита Осипцов <osipczovnikita@gmail.com>
2022-06-10 04:08:18 +0300

    hopefully done cw

--- cw/EfficiencyMarking/Source.cpp
+++ cw/EfficiencyMarking/Source.cpp
@@ -1,58 +1,93 @@
 #include <iostream>
+#include <fstream>
 #include <vector>
 #include <algorithm>
+#include <chrono>
 
 #include "Tree.h"
+
+using namespace std::chrono;
+
+Tree treesXor(Tree& first, Tree& second) { // result = first xor second
+	auto result = Tree();
+
+	for (auto i : first)
+		if (!second.contains(i))
+			result.insert(i);
+
+	for (auto i : second)
+		if (!first.contains(i))
+			result.insert(i);
+
+	return std::move(result);
+}
+
+Tree treesMinus(const Tree& first, const Tree& second) { // result = first / second
+	auto result = Tree(first);
+	for (auto i : second)
+		result.remove(i);
+
+	return std::move(result);
+}
+
+Tree treesOr(const Tree& first, const Tree& second) { // result = first or second
+	auto result = Tree(first);
+	result.insert(second.begin());
+	return std::move(result);
+}
 
 Tree generateRandomTree(int size) {
 	auto vect = std::vector<int>(size);
 
-	for (int i = 0; i < size; ++i)
-		vect[i] = rand();
-	std::sort(vect.begin(), vect.end());
+	vect[0] = rand();
+	for (int i = 1; i < size; ++i)
+		vect[i] = vect[i - 1] + rand();
 
 	return std::move(Tree(vect));
 }
 
 int main() {
-	auto vect = std::vector<int>({ 5, 7, 12, 19, 24, 31, 40, 53, 68, 75, 84, 91 });
-	auto vect1 = std::vector<int>({5, 12, 21, 657, 11224});
-	auto tree = Tree(vect);
+	const int minSize = 10;
+	const int maxSize = 200;
+	const int sizeStep = 10;
+	const bool debugMode = false;
 
-	//Тест subst
-	/*std::cout << "\n" << tree << "\n\n";
-	auto ntree = Tree(vect);
-	auto treesubst = tree;
-	treesubst.insert(ntree.begin() + 3);
-	std::cout << "\n" << treesubst << "\n\n";*/
+	Tree a, b, c, d, e;
+	Tree dMinusE;
+	Tree cXorUpper;
+	Tree aOrB;
+	Tree result;
 
-	//Тест вставки
-	/*for (int i = -10; i <= 100; i += 10) {
-		tree.insert(i);
-		std::cout << "\n" << tree << '\n';
+	std::ofstream stream("in.txt");
+
+	//srand(time(nullptr));
+
+	for (int size = minSize; size <= maxSize; size += sizeStep) {
+		a = generateRandomTree(size);
+		b = generateRandomTree(size);
+		c = generateRandomTree(size);
+		d = generateRandomTree(size);
+		e = generateRandomTree(size);
+
+		if (debugMode) std::cout << "\na\n" << a << "\nb\n" << b << "\nc\n" << c << "\nd\n" << d << "\ne\n" << e << "\n\n";
+		
+		auto start = std::chrono::high_resolution_clock::now();
+
+		dMinusE = treesMinus(d, e);
+		if (debugMode) std::cout << "d minus e" << dMinusE << "\n\n";
+		cXorUpper = treesXor(c, dMinusE);
+		if (debugMode) std::cout << "c xor (d minus e)" << cXorUpper << "\n\n";
+		aOrB = treesOr(a, b);
+		if (debugMode) std::cout << "a or b" << aOrB << "\n\n";
+		result = treesOr(aOrB, cXorUpper);
+		if (debugMode) std::cout << "result" << result << "\n\n";
+
+		auto end = std::chrono::high_resolution_clock::now();
+		auto dt = duration_cast<duration<double>>(end - start);
+		stream << size << " " << dt.count() << "\n";
 	}
-	std::cout << "\n" << tree << "\n\n";*/
 
-	// Тест инкремента и декремена итератора
-	/*auto iter = tree.begin();
-	for (; iter != tree.end(); ++iter)
-		std::cout << *iter << " ";
-	std::cout << "\n";
-	for (; iter != tree.begin(); --iter)
-		std::cout << *iter << " ";
-	std::cout << *iter << " \n";
-	for (; iter != tree.end(); ++iter)
-		std::cout << *iter << " ";*/
-
-	//Тест заполнения пустого дерева
-	Tree empty;
-	for (int i = 0; i < 30; ++i) {
-		std::cout << empty;
-		empty.insert(i);
-	}
-	std::cout << empty;
-
-	//Тест вставки по значению
+	stream.close();
 
 	return 0;
 }
--- pract3/STLUsing/Source.cpp
+++ pract3/STLUsing/Source.cpp
@@ -3,6 +3,45 @@
 
 #include "Tree.h"
 
+Tree generateRandomTree(int size) {
+	auto vect = std::vector<int>(size);
+
+	vect[0] = rand();
+	for (int i = 1; i < size; ++i)
+		vect[i] = vect[i-1] + rand();
+	//std::sort(vect.begin(), vect.end());
+
+	return std::move(Tree(vect));
+}
+
+Tree treesXor(Tree& first, Tree& second) { // result = first xor second
+	auto result = Tree();
+
+	for (auto i : first)
+		if (!second.contains(i))
+			result.insert(i);
+
+	for (auto i : second)
+		if (!first.contains(i))
+			result.insert(i);
+
+	return std::move(result);
+}
+
+Tree treesExclude(const Tree& first, const Tree& second) { // result = first / second
+	auto result = Tree(first);
+	for (auto i : second)
+		result.remove(i);
+
+	return std::move(result);
+}
+
+Tree treesOr(const Tree& first, const Tree& second) { // result = first or second
+	auto result = Tree(first);
+	result.insert(second.begin());
+	return std::move(result);
+}
+
 int main() {
 	auto vect = std::vector<int>({ 5, 7, 12, 19, 24, 31, 40, 53, 68, 75, 84, 91 });
 	auto vect1 = std::vector<int>({5, 12, 21, 657, 11224});
@@ -32,16 +71,6 @@ int main() {
 	std::cout << *iter << " \n";
 	for (; iter != tree.end(); ++iter)
 		std::cout << *iter << " ";*/
-
-	//Тест заполнения пустого дерева
-	Tree empty;
-	for (int i = 0; i < 30; ++i) {
-		std::cout << empty;
-		empty.insert(i);
-	}
-	std::cout << empty;
-
-	//Тест вставки по значению
 
 	return 0;
 }
